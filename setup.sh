c822ee2653f4da78fa88d65eeda7b3350e8f9af9
Till Kleisli <till@kleisli.ch>
2022-12-22 10:30:20 +0100

    update ddev config

--- setup.sh
+++ setup.sh
@@ -20,6 +20,8 @@ find ./.ddev -type f -name 'config.yaml' | xargs sed -i "s/quickstart/${PROJECT_
 find . -type f -name 'composer.json' | xargs sed -i "s/quickstart\/site/${COMPOSER_PACKAGE_NAME}/g"
 find . -type f ! -name 'setup.sh' | xargs sed -i "s/Quickstart\.Site/${NEOS_PACKAGE_NAME}/g"
 
+ddev start
 ddev flow site:create ${PROJECT_NAME} ${NEOS_PACKAGE_NAME} ${NEOS_PACKAGE_NAME}:Document.HomePage
 echo "Configure admin user"
 ddev flow user:create --roles Administrator
+ddev describe
